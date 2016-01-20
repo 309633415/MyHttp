5be3f79f7c885d359051c2a9a6cf148ca19a8854
jiashubing <1310059060@qq.com>
2016-01-20 16:01:12 +0800

    删除添加jar包，以及调整buildpath的顺序，实现cxf框架下的webservice测试类

--- MyHttp/src/demoinfo/webservice/cxf/HelloWebServiceImpl.java
+++ MyHttp/src/demoinfo/webservice/cxf/HelloWebServiceImpl.java
@@ -1,7 +1,7 @@
 package demoinfo.webservice.cxf;
 
 import javax.jws.WebService;  
-@WebService(endpointInterface="demoinfo.webservice.environment.HelloWebService")  
+@WebService(endpointInterface="demoinfo.webservice.cxf.HelloWebService")  
 public class HelloWebServiceImpl implements HelloWebService{  
     public String sayHello(String name) {  
         return "hello " + name;  
